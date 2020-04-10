e529b41dc2c2e3956bada33a1693524e6cc5f40a
wn0824 <63041856+wn0824@users.noreply.github.com>
2020-04-10 17:59:53 +0900

    test new menu
    change menu text
    
    add new menu : GuGuDan
    
    unsettled error : GuGuDan JS (write array = undefined , NaN, Dont alert.)

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ function New_join() {
     window.open("newjoin.html","newjoinpopup","width=400px, height= 400px, left=200, right=200");
   }
 
+
+function Create(){
+    var number = document.getElementById("gugudan").value;
+    var guguarray=[];
+    if(number == 0){
+        alert("chioce the number please!")
+    }else{
+        for(var i=1; i<=9;i++){
+        var numtext = number+"*"+i+"="+(number*i);
+        guguarray.push(numtext);
+        document.getElementById('gugubox').value = guguarray
+        }                    
+    }
+}
